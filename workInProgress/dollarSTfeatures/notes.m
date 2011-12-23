99d27de96a3aa81f8717a38a17e02bf4fa18ffb4
Richard T. Guy <guy@cs.toronto.edu>
2011-12-23 17:55:17 -0500

    added explanation of dollars stfeature method output.

---
+++ workInProgress/dollarSTfeatures/notes.m
@@ -0,0 +1,11 @@
+% Using the function call
+
+% [R,subs,vals,cuboids,V] = stfeatures(grayVid,2, 3, 1, 2e-4, [], 1.85, 1, 1, 0 );
+%
+% In this return set:
+%    R is the set of image frames and the detector strength at each image
+%    subs(i,:) is the set of center points in space-time of the i'th
+%      cuboid.
+%    vals(i) is the value of the metric at that point.
+%    cubiods(:,:,:,i) is the value set at the cuboid.
+%    V is an optional image.
